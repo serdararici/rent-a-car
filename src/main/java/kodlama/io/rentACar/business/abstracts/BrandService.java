b2acf9dbaba86529a77c2e21d11972adcaa18e02
Serdar Arici <serdararici3@gmail.com>
2025-06-19 18:32:28 +0300

    Initialize rentACar database and insert sample data into brands table. Create request and response packages under business layer. Add CreateBrandRequest and GetAllBrandsResponse DTO classes. Update BrandManager to map Brand entities to GetAllBrandsResponse using stream mapping in getAll method.

--- src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
+++ src/main/java/kodlama/io/rentACar/business/abstracts/BrandService.java
@@ -2,9 +2,9 @@ package kodlama.io.rentACar.business.abstracts;
 
 import java.util.List;
 
-import kodlama.io.rentACar.entities.concretes.Brand;
+import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
 
 public interface BrandService {
-	List<Brand> getAll();
+	List<GetAllBrandsResponse> getAll();
 	
 }
