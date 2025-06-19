346c9024b16837b0fbbbcc69555cbfac9795b208
Serdar Arici <serdararici3@gmail.com>
2025-06-19 09:28:15 +0300

    Add BrandController with REST annotations in webapi.controllers package

--- src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -2,15 +2,19 @@ package kodlama.io.rentACar.business.concretes;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
 
+@Service  // this class is a Business object
 public class BrandManager implements BrandService{
 	private BrandRepository brandRepository;
 
 	
-	
+	@Autowired
 	public BrandManager(BrandRepository brandRepository) {
 		super();
 		this.brandRepository = brandRepository;
--- src/main/java/kodlama/io/rentACar/dataAccess/abstracts/BrandRepository.java
+++ src/main/java/kodlama/io/rentACar/dataAccess/abstracts/BrandRepository.java
@@ -2,8 +2,11 @@ package kodlama.io.rentACar.dataAccess.abstracts;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import kodlama.io.rentACar.entities.concretes.Brand;
 
+@Repository
 public interface BrandRepository {
 	List<Brand> getAll();
 }
