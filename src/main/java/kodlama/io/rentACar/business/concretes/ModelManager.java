cc90730b7fa7a280927197ea961029c6c0290d2a
Serdar Arici <serdararici3@gmail.com>
2025-06-30 10:10:29 +0300

    Add createModel functionality with DTO support
    - Create CreateModelRequest DTO in requests package
    - Implement add() method in ModelManager using ModelMapper to map
    request to entity

--- src/main/java/kodlama/io/rentACar/business/concretes/ModelManager.java
+++ src/main/java/kodlama/io/rentACar/business/concretes/ModelManager.java
@@ -6,11 +6,10 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 import kodlama.io.rentACar.business.abstracts.ModelService;
-import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.requests.CreateModelRequest;
 import kodlama.io.rentACar.business.responses.GetAllModelsResponse;
-import kodlama.io.rentACar.dataAccess.abstracts.ModelRepository;
 import kodlama.io.rentACar.core.utilities.mappers.ModelMapperService;
-import kodlama.io.rentACar.entities.concretes.Brand;
+import kodlama.io.rentACar.dataAccess.abstracts.ModelRepository;
 import kodlama.io.rentACar.entities.concretes.Model;
 import lombok.AllArgsConstructor;
 
@@ -30,6 +29,14 @@ public class ModelManager implements ModelService{
 						.map(model, GetAllModelsResponse.class)).collect(Collectors.toList());
 		
 		return modelsResponse;
+	}
+
+	@Override
+	public void add(CreateModelRequest createModelRequest) {
+		Model model = this.modelMapperService.forRequest().map(createModelRequest, Model.class);
+				
+		this.modelRepository.save(model);
+		
 	}
 
 }
