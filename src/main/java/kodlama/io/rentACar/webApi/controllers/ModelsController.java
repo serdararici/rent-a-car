f50c6a53d05e2b555d01ab98b6e17261dc3b8a65
Serdar Arici <serdararici3@gmail.com>
2025-07-06 23:22:38 +0300

    Add validation error handling using ValidationProblemDetails
    * Create ValidationProblemDetails class to hold field-specific error
    messages
    * Handle MethodArgumentNotValidException with @ExceptionHandler in
    RentACarApplication
    * Return BAD_REQUEST with field errors in a structured format
    * Enable validation in controller by adding @Valid to request parameters

--- src/main/java/kodlama/io/rentACar/webApi/controllers/ModelsController.java
+++ src/main/java/kodlama/io/rentACar/webApi/controllers/ModelsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import kodlama.io.rentACar.business.abstracts.ModelService;
 import kodlama.io.rentACar.business.requests.CreateModelRequest;
 import kodlama.io.rentACar.business.responses.GetAllModelsResponse;
@@ -28,7 +29,7 @@ public class ModelsController {
 	
 	@PostMapping()
 	@ResponseStatus(code=HttpStatus.CREATED)
-	public void add(@RequestBody() CreateModelRequest createModelRequest) {
+	public void add(@RequestBody() @Valid() CreateModelRequest createModelRequest) {
 		this.modelService.add(createModelRequest);
 	}
 }
